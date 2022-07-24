40192fc799d546d80556676486670ff8b1f9925a
Ramya Rajasekaran <ramya.rajasekaran@ramyara-wsm4g3b.internal.salesforce.com>
2022-07-24 15:11:13 -0700

    works like a hacky LingQ

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-console.log("Ramya, you're starting here!");
+console.log("This is from background.js");
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -13,21 +13,6 @@ var db = firebase.firestore();
 var knownWordsRef = db.collection("known_words");
 var unknownWordsRef = db.collection("unknown_words");
 
-
-// knownWordsRef.doc("தண்டனை").set({
-//     word: "தண்டனை",
-//     meanings: ["penalty", "punishment, impunity"] });
-// knownWordsRef.doc("அலை").set({
-//     word: "அலை",
-//     meanings: ["wave", "shake", "sea", "stray about", "wander"] });
-
-// knownWordsRef.get().then((querySnapshot) => {
-//     querySnapshot.forEach((doc) => {
-//         // doc.data() is never undefined for query doc snapshots
-//         console.log(doc.id, " => ", doc.data());
-//     });
-// });
-
 // Inject the payload.js script into the current tab after the popout has loaded
 window.addEventListener('load', function (evt) {
     chrome.tabs.executeScript(null, { file: "content_script.js" });
@@ -36,14 +21,10 @@ window.addEventListener('load', function (evt) {
 // Listen to messages from the content_script.js script
 // On Click word is added to known list in DB
 chrome.runtime.onMessage.addListener(function (message) {
-	
     
-    console.log('From background.js: ' + message);
+    console.log('Word: ' + message.word);
+    console.log('Meaning: ' + message.meaning);
 
-    // TODO: Use lookup dictionary or API to get meaning
-    // knownWordsRef.doc(message).set({
-    // word: message,
-    // meanings: ["test_meaning1", "test_meaning2"] });
-
+    console.log("added to DB");
 });
 
--- content_script.js
+++ content_script.js
@@ -1,19 +1,87 @@
-console.log('Content script is a go')
+/* ***************  HOW TO USE TAMIL READER **************
+ * HOW HIGHLIGHT COLORS WORK:
+ * all UNKNOWN (new) words highlighted in BLUE
+ * all KNOWN words not highlighted
+ * all words SEEN before but not yet known highlighted in YELLOW
+ * 
+ * TO LOOKUP A WORD:
+ * To see the meaning of a word, click on it. Meaning will be shown in a popup box.
+ * You can click the same word for popup to disappear.
+ * 
+ * TO LEARN A WORD:
+ * Once you've learned a word, you can mark it as KNOWN by
+ * 1. Clicking word to show meaning
+ * 2. Hitting '+' button in the lower right corner
+ * 3. Close meaning box by clicking word again
+ */
+var uniqueId = 0; // variable for generating unique ID for popup div
+var isShown = false; // var to keep track of whether popup is showing or not
 
-let popup = document.createElement("div");
-popup.id = "popup";
-popup.title = "";
-popup.hidden="hidden";
+const API_KEY=""; //GTranslate
+
+async function translate(text) {
+    let res = await axios.post(
+        `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`,
+        { q: text, target: "en", source: "ta" }
+        );
+        let translation = res.data.data.translations[0].translatedText;
+        return translation;
+}
+
+class Word {
+    constructor (word, meaning){
+        this.word = word;
+        this.meaning = meaning;
+    }
+    toString() {
+        return this.word + ' means ' + this.meaning;
+    }
+}
+
+var wordConverter = {
+    toFirestore: function(data) {
+        return {
+            word: data.word,
+            meaning: data.meaning
+            };
+    },
+    fromFirestore: function(snapshot, options){
+        const data = snapshot.data(options);
+        return new Word(data.word, data.meaning);
+    }
+}
+
+function addFloatingButton(){
+    var outerDiv = document.createElement("div");
+    outerDiv.classList.add("fab-container");
+
+    var innerDiv = document.createElement("div");
+    innerDiv.classList.add("button", "iconbutton");
+
+    innerDiv.innerHTML = '<i class="fa-solid fa-plus" id="addButton"></i>';
+
+    outerDiv.appendChild(innerDiv);
+    document.body.appendChild(outerDiv);
+}
+
+function addPopup(para){
+    var popup = document.createElement("div");
+    popup.title = "";
+    popup.id = uniqueId++ + "popup";
+    popup.classList.add("popuptext");
+
+    para.classList.add("popup");
+    para.appendChild(popup);
+}
 
 const firebaseConfig = {
-
-};
-
-var meaning_map = {
-    "அரசர்" : "king",
-    "ஏன்" : "why",
-    "அந்த" : "that",
-    "தெனாலிராமன்" : "Tenali Raman"
+    apiKey: "AIzaSyDeSlFnzC9u6gboHrpgvD2schGIg4TQKEg",
+    authDomain: "tamil-reader.firebaseapp.com",
+    projectId: "tamil-reader",
+    storageBucket: "tamil-reader.appspot.com",
+    messagingSenderId: "508821920207",
+    appId: "1:508821920207:web:1f21f886c117edeca8daba",
+    measurementId: "G-D9CNW7NNT4"
 };
 
 // Initialize Firebase
@@ -24,94 +92,224 @@ var knownWordsRef = db.collection("known_words");
 var unknownWordsRef = db.collection("unknown_words");
 
 
-$(document).ready(function() {
-    console.log('trying to letter');
+$(document).ready(function () {
+    addFloatingButton();
+
     $("p").lettering('words');
     // $("a").lettering('words');
     // $("h1").lettering('words');
     // $("h3").lettering('words');
 
-    //scanUnknownWords();
+    var allWords = document.querySelectorAll("[class^=word]")
+    for (word of allWords) {
+        addPopup(word);
+        isWordUnknown(word);
+    }
 
-    //alert('going to popup after');
+    // SHOW MEANING
+    $(".popup").click(function (e) {
 
-    // var someP = document.getElementsByTagName('p')[0];
-    // someP.appendChild(popup);
+        var $this = $(this)
+        var word = this.innerText;
 
-    // $( "#popup" ).dialog({
-    //     autoOpen: false
-    //   });
+        var punctuationless = word.replace(/[.,\/#!\?\"$%\^&\*;:{}=\-_`~()]/g, "");
+        var finalWord = punctuationless.replace(/\s{2,}/g, " ");
 
-    // TODO: Hover to show meaning?
-    $("[class^=word]").hover(function () {
+        let res = finalWord.split("\n");
 
-       for (elt of document.getElementsByClassName(this.className)) {
-           if (elt.innerText === this.innerText){ 
-               console.log('Meaning of ' + this.innerText);
-           }
-       }
+        var currentDialog = document.getElementById($this[0].lastElementChild.id);
+
+        if (res.length == 1) {
+            if (isShown) {
+                currentDialog.classList.remove("show");
+                this.classList.remove("link");
+                isShown = false;
+            } else {
+                knownWordsRef.doc(res[0])
+                .withConverter(wordConverter)
+                .get().then((doc) => {
+                    if (doc.exists) {
+                        var word = doc.data();
+
+                        console.log("Meaning of " + res[0] + " from known DB: ", word.toString());
+                        currentDialog.innerText = word.meaning;
+                        currentDialog.classList.add("show");
+                        this.classList.add("link");
+                        isShown = true;
+                    }
+                    else {
+                        unknownWordsRef.doc(res[0])
+                            .withConverter(wordConverter)
+                            .get().then((doc) => {
+                                if (doc.exists) {
+                                    var word = doc.data();
+        
+                                    // word has valid meaning
+                                    if(word.meaning !== "empty_meaning") {
+                                        console.log("Meaning of " + res[0] + " from unknown DB: ", word.toString());
+                                        currentDialog.innerText = word.meaning;
+                                        currentDialog.classList.add("show");
+                                        this.classList.add("link");
+                                        isShown = true;
+
+                                        this.classList.remove("unknown");
+                                        this.classList.add("seen");
+                                    } 
+                                    else { // word does not have valid meaning yet, so we need to look it up
+                                        console.log("GTranslate API called to translate: ", finalWord);
+                                        translate(finalWord).then(
+                                            (data) => {
+                                                currentDialog.innerText = data;
+                                                var translation = new Word(finalWord, currentDialog.innerText);
+                                                // Adding to unknown DB, resetting the meaning to be valid
+                                                unknownWordsRef.doc(finalWord)
+                                                    .withConverter(wordConverter)
+                                                    .set(translation);
+
+                                                this.classList.remove("unknown");
+                                                this.classList.add("seen");
+                                            }
+                                        );
+                                        currentDialog.classList.add("show");
+                                        this.classList.add("link");
+                                        isShown = true;
+                                    }
+
+                                    // scan doc for all unknown occurences of this word and add 'SEEN' highlight
+                                    for (elt of document.getElementsByClassName('unknown')) {
+                                        if (elt.innerText === word) {
+                                            elt.classList.remove("unknown");
+                                            elt.classList.add("seen");
+                                        }
+                                    }
+                                }
+                            }).catch((error) => {
+                                console.log("Error getting document:", error);
+                            });
+                    }
+                }).catch((error) => {
+                    console.log("Error getting document:", error);
+                });   
+            }
+        } else {
+            currentDialog.classList.remove("show");
+            this.classList.remove("link");
+            isShown = false;
+        }
+
     });
 
-    // TODO: Click to add the word from unknown to known?
-    $("[class^=word]").click(function(e){
+    // adding word to KNOWN from UNKOWN
+    $("#addButton").click(function (e) {
+    
+        let shownElement = document.querySelectorAll(".show");
+        let meaning = shownElement[0].innerText
 
-        var $this = $( this ),
-        _parent = $this.parent("p");
-        _parent.addClass("parent");
-        for (elt of document.getElementsByClassName(this.className)) {
-            if (elt.innerText === this.innerText){ 
+        let parentHTML =  shownElement[0].parentNode.innerHTML;
+        let word = parentHTML.split("<div")[0];
+        var punctuationless = word.replace(/[.,\/#!\?\"$%\^&\*;:{}=\-_`~()]/g,"");
+        var finalWord = punctuationless.replace(/\s{2,}/g," ");
 
-               
-                var spanTag = document.querySelector(this.className);
-                var pTag = document.querySelector(".parent");
-                console.log('need to insert popup first');
-                console.log('popup', popup);
-                console.log('pTag', pTag);
-                console.log('spanTag', spanTag);
+        let translation = new Word(finalWord, meaning);
 
-                pTag.insertBefore(popup, spanTag);
-                console.log('added popup to DOM');
+        // removing word from UNKNOWN db
+        unknownWordsRef.doc(finalWord)
+        .withConverter(wordConverter)
+        .delete().then((doc) => {
+            console.log("Removed " + finalWord + " from UNKNOWN db");
+        }).catch((error) => {
+            console.log("Error deleting document:", error);
+        });
 
-                var word = this.innerText;
+        // moving from UNKNOWN db to KNOWN db
+        console.log("adding to KNOWN DB: ", translation);
+        knownWordsRef.doc(finalWord)
+            .withConverter(wordConverter)
+            .set(translation);
 
-                var punctuationless = word.replace(/[.,\/#!\?\"$%\^&\*;:{}=\-_`~()]/g,"");
-                var finalWord = punctuationless.replace(/\s{2,}/g," ");
+        shownElement[0].parentNode.classList.remove("unknown");
+        shownElement[0].parentNode.classList.remove("seen");
+        shownElement[0].parentNode.classList.add("known");
 
-                console.log('Adding to known:  ', finalWord);
-
-                e.stopPropagation();
-
-                console.log('Trying to open dialog');
-                var dialogContent = meaning_map[finalWord.toLowerCase()];
-                if(dialogContent && dialogContent.length > 0) {
-                    $( "#popup" ).dialog({
-                        "autoOpen": false,
-                        "modal": true,
-                        "title": finalWord
-                    }).html(dialogContent);
-
-                    $( "#popup" ).dialog('open');
-                    console.log('after');
-                }
-
-                
-                chrome.runtime.sendMessage(finalWord);
+        // scan doc for all occurences of this word and remove highlight
+        for (elt of document.getElementsByClassName('seen')) {
+            if (elt.innerText === finalWord){ 
+                elt.classList.remove("unknown");
+                elt.classList.remove("seen");
+                elt.classList.add("known");
             }
         }
-        
-         });
-         
+
+        for (elt of document.getElementsByClassName('unknown')) {
+            if (elt.innerText === finalWord){ 
+                elt.classList.remove("unknown");
+                elt.classList.remove("seen");
+                elt.classList.add("known");
+            }
+        }
+
+    });
 });
 
-function scanUnknownWords() {
-    // unknownWordsRef.doc(finalWord).set({
-    //     word: finalWord,
-    //     meanings: ["test_meaning1", "test_meaning2"] });
-    // console.log('from content_Script.js: ')
-    // knownWordsRef.get().then((querySnapshot) => {
-    //     querySnapshot.forEach((doc) => {
-    //         // doc.data() is never undefined for query doc snapshots
-    //         console.log(doc.id, " => ", doc.data());
-    //     });
-    // });
+// all UNKNOWN (new) words highlighted in BLUE
+function isWordUnknown(wordElement) {
+
+    var cleanedWord = wordElement.innerText;
+    var punctuationless = cleanedWord.replace(/[.,\/#!\?\"$%\^&\*;:{}=\-_`~()]/g,"");
+    var finalWord = punctuationless.replace(/\s{2,}/g," ");
+
+    if(finalWord === ""){
+        // Adding this bc it breaks without lol
+        return 0;
+    }
+
+    var englishRegExp = /[a-zA-Z]/g;             
+    if(englishRegExp.test(finalWord)){
+        // this is an english word. we want to ignore this.
+        return 0;
+    }
+
+    knownWordsRef.doc(finalWord)
+        .withConverter(wordConverter)
+        .get().then((doc) => {
+            if (doc.exists) {
+                // we know the word
+                // no need to highlight
+                if (wordElement.classList.item(0).startsWith('word')) {
+                    wordElement.classList.remove(wordElement.classList.item(0));
+                    wordElement.classList.add("known");
+                } 
+            } else {
+                // we don't know the word yet
+                unknownWordsRef.doc(finalWord)
+                .withConverter(wordConverter)
+                .get().then((doc) => {
+                    if (doc.exists) {
+                        // mark as yellow because we have seen this before
+                        if (wordElement.classList.item(0).startsWith('word')) {
+                            wordElement.classList.remove(wordElement.classList.item(0));
+                            wordElement.classList.remove("unknown");
+                            wordElement.classList.add("seen");
+                        }
+                    } else {
+                        // change highlight of unknown word
+                        if (wordElement.classList.item(0).startsWith('word')) {        
+                            wordElement.classList.remove(wordElement.classList.item(0));
+                            wordElement.classList.add("unknown");
+                        } 
+        
+                        // adding to unknown db
+                        var translation = new Word(finalWord, "empty_meaning");
+                        unknownWordsRef.doc(finalWord)
+                        .withConverter(wordConverter)
+                        .set(translation);
+        
+                    } 
+                }).catch((error) => {
+                    console.log("Error getting document:", error);
+                });
+            }
+        }).catch((error) => {
+            console.log("Error getting document:", error);
+        });
 }
